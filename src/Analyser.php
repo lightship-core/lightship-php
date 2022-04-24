3625380df7581b2639fc9caf8c82fbbf291aece6
khalyomede <khalyomede@gmail.com>
2022-04-24 11:30:00 +0200

    add doctypeHtmlPresent rule

--- src/Analyser.php
+++ src/Analyser.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use GuzzleHttp\TransferStats;
 use Khalyomede\Rules\Accessibility\ButtonsAndLinksUseAccessibleName;
+use Khalyomede\Rules\Accessibility\DoctypeHtmlPresent;
 use Khalyomede\Rules\Accessibility\IdsAreUnique;
 use Khalyomede\Rules\Accessibility\ImagesHaveAltAttributes;
 use Khalyomede\Rules\Accessibility\MetaViewportPresent;
@@ -74,6 +75,7 @@ class Analyser
             ButtonsAndLinksUseAccessibleName::fromResponse($response)->toReport(),
             IdsAreUnique::fromResponse($response)->toReport(),
             ImagesHaveAltAttributes::fromResponse($response)->toReport(),
+            DoctypeHtmlPresent::fromResponse($response)->toReport(),
         ];
     }
 
---
+++ tests/Accessbility/DoctypeHtmlPresentTest.php
@@ -0,0 +1,79 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Khalyomede\Lightship;
+use Khalyomede\Rules\Accessibility\DoctypeHtmlPresent;
+use Khalyomede\RuleType;
+
+test("doctype html present is an accessibility rule", function (): void {
+    expect((new DoctypeHtmlPresent())->type())->toBe(RuleType::Accessibility);
+});
+
+test("doctype html passes if the document starts with DOCTYPE", function (): void {
+    $client = new Client([
+        "handler" => HandlerStack::create(new MockHandler([
+            new Response(200, [], "<!DOCTYPE html>")
+        ]))
+    ]);
+
+    $lightship = new Lightship($client);
+
+    $lightship->route("https://example.com")
+        ->analyse();
+
+    $data = $lightship->toArray();
+
+    assert(is_array($data[0]));
+
+    expect($data[0]["accessibility"][5])->toBe([
+        "name" => "doctypeHtmlPresent",
+        "passes" => true,
+    ]);
+});
+
+test("doctype html does not passes if the document does not start with DOCTYPE", function (): void {
+    $client = new Client([
+        "handler" => HandlerStack::create(new MockHandler([
+            new Response(200, [], "<html></html>")
+        ]))
+    ]);
+
+    $lightship = new Lightship($client);
+
+    $lightship->route("https://example.com")
+        ->analyse();
+
+    $data = $lightship->toArray();
+
+    assert(is_array($data[0]));
+
+    expect($data[0]["accessibility"][5])->toBe([
+        "name" => "doctypeHtmlPresent",
+        "passes" => false,
+    ]);
+});
+
+test("doctype html does not passes if the document is empty", function (): void {
+    $client = new Client([
+        "handler" => HandlerStack::create(new MockHandler([
+            new Response(200, [], "")
+        ]))
+    ]);
+
+    $lightship = new Lightship($client);
+
+    $lightship->route("https://example.com")
+        ->analyse();
+
+    $data = $lightship->toArray();
+
+    assert(is_array($data[0]));
+
+    expect($data[0]["accessibility"][5])->toBe([
+        "name" => "doctypeHtmlPresent",
+        "passes" => false,
+    ]);
+});
